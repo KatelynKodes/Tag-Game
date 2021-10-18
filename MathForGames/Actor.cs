5282b08dd000f94d64f8d98d65d2eb18c56b84d4
KatelynKodes <88592715+KatelynKodes@users.noreply.github.com>
2021-10-18 15:37:16 -0500

    Initialize RayLib

---
+++ MathForGames/Actor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    struct Icon
+    {
+        public char Symbol;
+        public Color color;
+    }
+
+    class Actor
+    {
+        private Icon _icon;
+        private string _name;
+        private Vector2 _position;
+        private bool _started;
+
+        public bool Started
+        {
+            get { return _started; }
+        }
+
+        public Vector2 GetPosition
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public Icon GetIcon
+        {
+            get { return _icon; }
+        }
+
+        public string GetName
+        {
+            get { return _name; }
+        }
+
+        public Actor(char icon, float x, float y, Color IconColor, string name = "Actor") :
+            this(icon, new Vector2 { X = x, Y = y }, IconColor, name)
+        { }
+
+        public Actor(char CharIcon, Vector2 Position, Color IconColor, string name = "Actor")
+        {
+            _icon = new Icon { Symbol = CharIcon, color = IconColor };
+            _position = Position;
+            _name = name;
+        }
+
+        public virtual void Start()
+        {
+            _started = true;
+        }
+
+        public virtual void Update(float deltaTime)
+        {
+            Console.WriteLine(GetName + ": (" + GetPosition.X + "," + GetPosition.Y + ")");
+        }
+
+        public virtual void Draw()
+        {
+            Raylib.DrawText(_icon.Symbol.ToString(), (int)GetPosition.X, (int)GetPosition.Y, 50, _icon.color);
+        }
+
+        public void End()
+        {
+
+        }
+
+        public virtual void OnCollision(Actor actor)
+        {
+        }
+    }
+}
---
+++ MathForGames/Engine.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using MathLibrary;
+using Raylib_cs;
+using System.Diagnostics;
+
+namespace MathForGames
+{
+    class Engine
+    {
+        public static bool _applicationShouldClose = false;
+        private static int _currentSceneIndex = 0;
+        private static Scene[] _scenes = new Scene[0];
+        private static string _winnerName = "";
+        private Stopwatch _stopwatch = new Stopwatch();
+
+        /// <summary>
+        /// Called to begin the application
+        /// </summary>
+        public void Run()
+        {
+            //Call start for the entire application
+            Start();
+
+            float currTime = 0;
+            float lastTime = 0;
+            float deltaTime = 0;
+
+            //Loop until application is told to close
+            while (!Raylib.WindowShouldClose())
+            {
+                //Get how much time has passed since the application started
+                currTime = _stopwatch.ElapsedMilliseconds / 1000;
+
+                //Set deltatime to be the difference in time from the last time recorded to the current time recorded
+                deltaTime = currTime - lastTime;
+
+                Update(deltaTime);
+                Draw();
+                Thread.Sleep(150);
+
+                //Set the last time recorded to be the current time
+                lastTime = currTime;
+            }
+
+            //Calll at the end of the entire application.
+            End();
+        }
+
+        /// <summary>
+        /// Called when the application starts
+        /// </summary>
+        private void Start()
+        {
+            _stopwatch.Start();
+
+            //Create Window using raylib
+            Raylib.InitWindow(800, 450, "MathForgames");
+
+            //Starts the current scene
+            _scenes[_currentSceneIndex].Start();
+        }
+
+        /// <summary>
+        /// Called everytime the game loops
+        /// </summary>
+        private void Update(float deltaTime)
+        {
+            _scenes[_currentSceneIndex].Update(deltaTime);
+
+            while (Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+            }
+        }
+
+        /// <summary>
+        /// Called At the end of the application
+        /// </summary>
+        private void End()
+        {
+            _scenes[_currentSceneIndex].End();
+            Raylib.CloseWindow();
+            Console.Clear();
+            Console.WriteLine(_winnerName + "Is the winner of the race");
+            Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Called everytime the game loops to update visuals
+        /// </summary>
+        private void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            //Adds all actor icons to buffer
+            _scenes[_currentSceneIndex].Draw();
+
+            Raylib.EndDrawing();
+        }
+
+        /// <summary>
+        /// Adds a new scene to the engines scene array
+        /// </summary>
+        /// <param name="scene"></param>
+        /// <returns></returns>
+        public int AddScene(Scene scene)
+        {
+            //Create Temporary Array
+            Scene[] TempArray = new Scene[_scenes.Length + 1];
+
+            //Copy all values into temporary array
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                TempArray[i] = _scenes[i];
+            }
+
+            //Set the last index to be the new scene
+            TempArray[_scenes.Length] = scene;
+
+            //set the old array to the new array
+            _scenes = TempArray;
+
+            //Return the last index
+            return _scenes.Length - 1;
+        }
+
+
+        /// <summary>
+        /// Gets the next key in the input stream
+        /// </summary>
+        /// <returns>The key that was pressed</returns>
+        public static ConsoleKey GetConsoleKey()
+        {
+            //If there is No Key being pressed...
+            if (!Console.KeyAvailable)
+            {
+                //...Return
+                return 0;
+            }
+
+            //Return the current key being pressed
+            return Console.ReadKey(true).Key;
+        }
+
+        //Closes the Application
+        public static void CloseApplication()
+        {
+            _applicationShouldClose = true;
+        }
+
+        public static void ChangeWinnerName(string newName)
+        {
+            _winnerName = newName;
+        }
+
+        /// <summary>
+        /// Knocks an opposing actor back a space if the player collides with it
+        /// </summary>
+        /// <param name="opponent"></param>
+        public static void KnockOpponentBack(Actor opponent)
+        {
+            //How much actor is knocked back
+            Vector2 KnockbackValue = new Vector2 { X = 1, Y = 0 };
+
+            //Subtract the current position by that knockback value
+            opponent.GetPosition -= KnockbackValue;
+        }
+    }
+}
